7bb0899fef029d9252038ae9cc757e13b29c511c
pp06121992 <162327684+pp06121992@users.noreply.github.com>
2024-03-18 15:42:44 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# func-pp06
+
+A Node.js module with functions for func-pp06 and more.
+
+## Installation
+
+You can install this module via npm: `npm install func-pp06`
+
+## Usage
+```javascript
+const fizzbuzz = require('fizzbuzz-magic');
+
+// Check if a number is prime
+console.log(fizzbuzz.isPrime(7)); // true
+
+// Print FizzBuzz up to a certain number
+fizzbuzz.fizzBuzz(15);
+
+// Reverse a string
+console.log(fizzbuzz.reverseString('hello')); // 'olleh'
+```
+
---
+++ index.js
@@ -0,0 +1,32 @@
+const isPrime = (num) => {
+  if (num <= 1) return false;
+  if (num <= 3) return true;
+
+  if (num % 2 === 0 || num % 3 === 0) return false;
+
+  let i = 5;
+  while (i * i <= num) {
+    if (num % i === 0 || num % (i + 2) === 0) return false;
+    i += 6;
+  }
+  return true;
+};
+
+const fizzBuzz = (n) => {
+  for (let i = 1; i <= n; i++) {
+    let output = '';
+    if (i % 3 === 0) output += 'Fizz';
+    if (i % 5 === 0) output += 'Buzz';
+    console.log(output || i);
+  }
+};
+
+const reverseString = (str) => {
+  return str.split('').reverse().join('');
+};
+
+module.exports = {
+  isPrime,
+  fizzBuzz,
+  reverseString
+};
